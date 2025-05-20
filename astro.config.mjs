94c1f96276592a31768c09e21949674a50a94fc1
Matija Tudan <mtudan@Matijas-MacBook-Pro.local>
2025-05-20 18:32:30 +0200

    Update domains to *.nulix.io
    Change *.nulix.hr to *.nulix.io
    
    Signed-off-by: Matija Tudan <matija.tudan@nulix.hr>

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://nulix.hr",
+  site: "https://nulix.io",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
